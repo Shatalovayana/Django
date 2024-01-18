43de1173560067330e25b2a1ffa13205eabcc451
Yana <yanashat198@gmail.com>
2024-01-18 22:44:50 +0700

    add settings for Category

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,15 +1,18 @@
 from django.urls import path
+from django.views.decorators.cache import cache_page
 
 from catalog.apps import CatalogConfig
 from catalog.views import contact, ProductListView, ProductDetailView, ProductCreateView, ProductUpdateView, \
-    ProductDeleteView
+    ProductDeleteView, CategoryDetailView, CategoryListView
 
 app_name = CatalogConfig.name
 urlpatterns = [
     path('', ProductListView.as_view(), name='index'),
     path('contacts/', contact),
-    path('product/<int:pk>/', ProductDetailView.as_view(), name='product'),
+    path('product/<int:pk>/', cache_page(60)(ProductDetailView.as_view()), name='product'),
     path('create/', ProductCreateView.as_view(), name='create_product'),
     path('update/<int:pk>/', ProductUpdateView.as_view(), name='update_product'),
-    path('delete/<int:pk>/', ProductDeleteView.as_view(), name='delete_product')
+    path('delete/<int:pk>/', ProductDeleteView.as_view(), name='delete_product'),
+    path('detail/<int:pk>/', cache_page(60)(CategoryDetailView.as_view()), name='detail_category'),
+    path('list/', CategoryListView.as_view(), name='list_category')
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,12 +1,13 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.forms import inlineformset_factory
-from django.http import Http404, HttpResponse
-from django.shortcuts import render, get_object_or_404
+from django.http import Http404
+from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 
 from catalog.forms import ProductForm, VersionForm
-from catalog.models import Product, Version
+from catalog.models import Product, Version, Category
+from catalog.services import get_cached_categories
 
 
 class ProductListView(ListView):
@@ -78,3 +79,25 @@ def contact(request):
         message = request.POST.get('message')
         print(f'{name}, {phone}, {message}')
     return render(request, 'catalog/contact.html')
+
+
+class CategoryDetailView(DetailView):
+    model = Category
+    template_name = 'catalog/category_detail.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        category = self.get_object()
+        products = Product.objects.filter(category=category)
+        context['products'] = products
+        return context
+
+
+class CategoryListView(ListView):
+    model = Category
+    template_name = 'catalog/category_list.html'
+
+    def get_context_data(self, **kwargs):
+        context_data = super().get_context_data(**kwargs)
+        context_data['category'] = get_cached_categories()
+        return context_data
